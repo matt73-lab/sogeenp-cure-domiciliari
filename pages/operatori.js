8eb61a82e4598f65258d29a245133c04f46fd7cf
matt73-lab <matteo.vannucci@gmail.com>
2025-07-17 13:04:23 +0200

    Update operatori.js

--- pages/operatori.js
+++ pages/operatori.js
@@ -1,1 +1,11 @@
+// pages/operatori.js
+import Layout from '../components/layout/Layout';
+import Operatori from '../components/modules/Operatori';
 
+export default function OperatoriPage() {
+  return (
+    <Layout>
+      <Operatori />
+    </Layout>
+  );
+}
